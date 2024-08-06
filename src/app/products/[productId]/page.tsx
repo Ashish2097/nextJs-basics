f8d80887afa45a2024e35d19075e5352e68368ad
Ashish Duklan <ashish.duklan@soroco.com>
2024-08-06 22:17:18 +0530

    Not found page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,10 @@
+import { notFound } from "next/navigation";
+
 export default function ProductDetails({ params }: { params: { productId: string }}) {
+  if (parseInt(params.productId) > 1000) {
+    notFound();
+  }
+
   return <h1>Details about product {params.productId}</h1>;
+
 }
